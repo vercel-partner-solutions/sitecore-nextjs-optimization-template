d4a8f91dafb20b80bffa09d949fbadb55d48a0d7
Alex <alex.hawley@vercel.com>
2025-03-29 17:42:31 -0500

    added next/font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,11 +6,27 @@ import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import 'assets/main.scss';
 
+import { Open_Sans, Roboto } from 'next/font/google';
+
+const openSans = Open_Sans({
+  subsets: ['latin', 'latin-ext', 'cyrillic', 'cyrillic-ext', 'greek', 'greek-ext', 'vietnamese'],
+  weight: ['300', '400', '600', '700', '800'],
+  style: ['normal', 'italic'],
+  display: 'swap',
+  variable: '--font-open-sans',
+});
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
 function App({ Component, pageProps }: AppProps<SitecorePageProps>): JSX.Element {
   const { dictionary, ...rest } = pageProps;
 
   return (
-    <>
+    <div className={`${openSans.variable} ${roboto.className}`}>
       <Bootstrap {...pageProps} />
       {/*
         // Use the next-localization (w/ rosetta) library to provide our translation dictionary to the app.
@@ -21,7 +37,7 @@ function App({ Component, pageProps }: AppProps<SitecorePageProps>): JSX.Element
         <Component {...rest} />
       </I18nProvider>
       <SpeedInsights />
-    </>
+    </div>
   );
 }
 
